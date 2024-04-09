edccb40fb3c2115523e86783a07543aa69f5edb9
“marijoroldan” <marijoroldan42@gmail.com>
2024-04-08 22:59:40 -0300

    cambios

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+int main () {
+    int *p_punt, numero=10;
+    p_punt=&numero;
+    printf("Hola mundo\n");
+    printf("contenido de la variable a la que apunta el puntero: %d\n",*p_punt);
+    printf("dirección de memoria almacenada por el puntero: %p\n",162,p_punt);
+    printf("dirección de memoria de la variable: %p\n",162,&numero);
+    printf("dirección de memoria del puntero: %p\n",162,&p_punt);
+    printf("tamaño de memoria usado por la variable numero: %d Bytes",164,sizeof(numero));
 
-int main(int argc, char const *argv[])
-{
-    printf("hola mundo");
     return 0;
 }
